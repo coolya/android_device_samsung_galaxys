346bf853f17e835f7aad571e72857e98b2fba864
Kolja Dummann <k.dummann@gmail.com>
2010-12-18 14:39:21 +0100

    fix config

--- full_galaxys.mk
+++ full_galaxys.mk
@@ -19,31 +19,14 @@
 # product configuration (apps).
 #
 
-# NFC is a product-level aspect that is too closely tied
-# to this device to justify putting in a more generic config,
-# but which is also not quite a low-level driver to put in
-# device.mk
-PRODUCT_PACKAGES := \
-        libnfc \
-        libnfc_jni \
-        Nfc \
-        Tag
-
-# Get the long list of APNs
-PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
-
-# Get the parts that are either non-open-source or depend
-# on non-open-source drivers
-$(call inherit-product-if-exists, vendor/samsung/galaxys/full_galaxys-trampoline.mk)
-
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # This is where we'd set a backup provider if we had one
 #$(call inherit-product, device/sample/products/backup_overlay.mk)
 $(call inherit-product, device/samsung/galaxys/device.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk)
 
 # Discard inherited values and use our own instead.
 PRODUCT_NAME := full_galaxys
 PRODUCT_DEVICE := galaxys
-PRODUCT_BRAND := Android
-PRODUCT_MODEL := Full Android on GalaxyS
+PRODUCT_MODEL := Full Android on Crespo
