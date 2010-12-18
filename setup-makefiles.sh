8ff1e8eaa81c89810a67719853d2863d90a16e0f
Daniel Hillenbrand <info@codeworkx.de>
2010-12-18 05:24:37 -0800

    setup-makefiles.sh: DEVICE=galaxys

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-DEVICE=crespo
+DEVICE=galaxys
 MANUFACTURER=samsung
 
 mkdir -p ../../../vendor/$MANUFACTURER/$DEVICE
