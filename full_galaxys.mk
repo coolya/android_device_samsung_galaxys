7e03d73f640045ff0381cfaa1f9ee338de4073a8
Daniel Hillenbrand <info@codeworkx.de>
2010-12-19 08:32:06 -0800

    full_galaxys.mk: PRODUCT_LOCALES += hdpi

--- full_galaxys.mk
+++ full_galaxys.mk
@@ -26,6 +26,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, device/samsung/galaxys/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk)
 
+# Galaxy S uses high-density artwork where available
+PRODUCT_LOCALES += hdpi
+
 # Discard inherited values and use our own instead.
 PRODUCT_NAME := full_galaxys
 PRODUCT_DEVICE := galaxys
