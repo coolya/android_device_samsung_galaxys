732355e43f691733cd51c03454bfd17955be3150
Daniel Hillenbrand <info@codeworkx.de>
2010-12-19 08:41:55 -0800

    BoardConfig.mk: BOARD_HAS_NO_MISC_PARTITION := true , BOARD_HAS_NO_RECOVERY_PARTITION := true

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -83,4 +83,6 @@ WIFI_DRIVER_MODULE_NAME     :=  "bcm4329"
 WIFI_DRIVER_MODULE_ARG      :=  "firmware_path=/vendor/firmware/fw_bcm4329.bin nvram_path=/vendor/firmware/nvram_net.txt"
 
 BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_HAS_NO_RECOVERY_PARTITION := true
 
